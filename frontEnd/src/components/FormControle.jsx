a393e0474a7e9a5562259016560aa798a84f8f3d
zsimo <mohamedzoraa100@gmail.com>
2023-07-09 02:57:57 +0100

    update

--- frontEnd/src/components/FormControle.jsx
+++ frontEnd/src/components/FormControle.jsx
@@ -23,7 +23,7 @@ const FormControle = ({id,value,setFormData,name,type}) => {
   return (
     <>
     <div className="relative w-full">
-        <label className={`text-white z-[-1] absolute left-4 transition-all ${(focused || !vide) ? ("top-0 translate-y-[-50%] text-[12px] bg-slate-800 px-1 z-20") : ("top-[50%] translate-y-[-50%] text-sm")}`} htmlFor="email">{name}</label>
+        <label className={`text-white z-[-1] absolute left-4 transition-all ${(focused || !vide) ? ("top-0 translate-y-[-50%] text-[12px] bg-slate-800 px-1 z-[10]") : ("top-[50%] translate-y-[-50%] text-sm")}`} htmlFor="email">{name}</label>
         <input onFocus={onFocus} onBlur={onBlur} className={`w-full ps-2 py-1 focus:outline-none rounded-md bg-transparent border-2 text-white text-[14px] ${focused ? ("border-gray-500"):("border-gray-700")}`} id={id} type={type==="password"?"password":"text"} value={value} onChange={handleChange}/>
     </div>
 </>
